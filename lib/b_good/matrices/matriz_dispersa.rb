6e859cbea61159f6f6283655563443c6b5219a34
Killian Jimenez <alu0100502053@ull.edu.es>
2013-11-25 21:56:38 +0000

    Cambiado los bucles por el metodo times

--- lib/b_good/matrices/matriz_dispersa.rb
+++ lib/b_good/matrices/matriz_dispersa.rb
@@ -1,18 +1,18 @@
 class Vector_disperso
-	attr_reader :vector
+  attr_reader :vector
 
-	def initialize(h = {})
+  def initialize(h = {})
     @vector = Hash.new(0)
     @vector = @vector.merge!(h)
   end
 
   def to_s
     @vector.to_s
- 	end
+  end
 
- 	def keys
- 		@vector.keys
- 	end
+  def keys
+    @vector.keys
+  end
 
   def hash
     @vector
@@ -26,80 +26,81 @@ class Vector_disperso
     @vector.merge!(other.hash){|key, oldval, newval| newval - oldval}
   end
 
- 	def []= (i,v)
- 		@vector[i] = v
- 	end
+  def []= (i,v)
+    @vector[i] = v
+  end
 
-	def [](i)
-    	@vector[i] 
-  	end
+  def [](i)
+    @vector[i] 
+  end
 end
 
 class Matriz_dispersa < Matriz
-	
-	attr_reader :matrix
+  attr_reader :matrix
 
-	def initialize(h = {})
-   	@matrix = Hash.new(0)
-    	for k in h.keys do 
-      	@matrix[k] = 	if h[k].is_a? Vector_disperso
-         						h[k]
-            				else 
-                     		@matrix[k] = Vector_disperso.new(h[k])
-                   	end
-    	end
-  	end
-
-  	def [](i)
-  		@matrix[i]
-  	end
-
-    def hash
-      @matrix
+  def initialize(h = {})
+    @matrix = Hash.new(0)
+      for k in h.keys do 
+        @matrix[k] = 	if h[k].is_a? Vector_disperso
+        h[k]
+      else 
+      	@matrix[k] = Vector_disperso.new(h[k])
+      end
     end
+  end
 
-  	def keys
-  		@matrix.keys
-  	end
+  def [](i)
+    @matrix[i]
+  end
 
-  	def col(j)
-    	c = {}
-    	for r in @matrix.keys do
-      	c[r] = @matrix[r].vector[j] if @matrix[r].vector.keys.include? j
-    	end
-    	Vector_disperso.new c
-  	end
+  def hash
+    @matrix
+  end
 
-	 def +(other)  #Para la suma de matrices
-	  ms = @matrix.clone
-	  ms.merge!(other.hash){ |key, oldval, newval| newval + oldval}
-   end
+  def keys
+    @matrix.keys
+  end
 
-   def -(other)  #Para la resta de matrices
+  def col(j)
+    c = {}
+    for r in @matrix.keys do
+      c[r] = @matrix[r].vector[j] if @matrix[r].vector.keys.include? j
+    end
+    Vector_disperso.new c
+  end
+
+  #Para la suma de matrices
+  def +(other)  
+    ms = @matrix.clone
+    ms.merge!(other.hash){ |key, oldval, newval| newval + oldval}
+  end
+
+  #Para la resta de matrices
+  def -(other)  
     ms = @matrix.clone
     ms.merge!(other.hash){ |key, oldval, newval| newval - oldval}
-   end
+  end
 
-   def *(other)  #Para la multiplicacion de matrices
-      ms = Hash.new(0)
-      h = Hash.new(0)
-      @mul = 0
-      # @matrix.keys.count.times do |k|
-      for k in @matrix.keys do
-      # other.hash.keys.count.times do |j|
-        for j in 0..other.hash.keys.count do
-        # @matrix[k].keys.count.times do |i|
-          for i in @matrix[k].keys do
-              if other.hash[i][j] != 0 then
-                @mul += @matrix[k][i] * other.hash[i][j]
-              end
+  #Para la multiplicacion de matrices
+  def *(other)  
+    ms = Hash.new(0)
+    h = Hash.new(0)
+    @mul = 0
+    @matrix.keys.count.times do |k|
+      @k =  @matrix.keys[k]
+      for j in 0..other.hash.keys.count do
+        @matrix[@k].keys.count.times do |i|
+          @i = @matrix[@k].keys[i]
+          if other.hash[@i][j] != 0 then
+            @mul += @matrix[@k][@i] * other.hash[@i][j]
           end
-          h[j] = @mul unless @mul == 0
-          @mul = 0
         end
-        ms[k] = h.clone unless h.empty?
-        h.clear
+        h[j] = @mul unless @mul == 0
+        @mul = 0
       end
-      ms2 = Matriz_dispersa.new(ms)
-   end
+      ms[@k] = h.clone unless h.empty?
+      h.clear
+    end
+    ms2 = Matriz_dispersa.new(ms)
+  end
 end
